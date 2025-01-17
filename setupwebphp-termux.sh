9f71b864e43653a47c0ce7bf914cf6fc91f5e46c
Snipavn <108266321+Snipavn@users.noreply.github.com>
2025-01-17 21:47:10 +0700

    Update setupwebphp-termux.sh

--- setupwebphp-termux.sh
+++ setupwebphp-termux.sh
@@ -10,18 +10,22 @@ echo "------------------------"
 echo "Lưu ý code chỉ dành cho termux"
 
 clear
-echo " Đang tải ngrok...."
+echo " Đang tải ngrok và các package khác...."
 pkg update -y
 pkg install git
 git clone https://github.com/Yisus7u7/termux-ngrok
 cd termux-ngrok
 bash install.sh
-echo "Đã Tải Xong"
+echo "Đã Tải Xong Ngrok"
+echo " Bắt đầu tải package "
+pkg install unzip
+apt install php
+apt install nginx
 cd
 mkdir PhpWebhost
 echo -e " ${YELLOW} Thư mục mới tạo tên là PhpWebhost "
 echo " Bắt đầu tải xuống.... "
 cd PhpWebhost && curl -O WebsitePHPBYSnipavn.zip https://github.com/Snipavn/Webhost-PHP/raw/refs/heads/main/WebsitePHPBYSnipavn.zip
-unzip
+unzip /PhpWebhost/WebsitePHPBYSnipavn.zip
 echo " Tải thành công "
 ls
