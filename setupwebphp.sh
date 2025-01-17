ffd2fc56c04c5977ab5338db172a1d96444b2d20
Snipavn <108266321+Snipavn@users.noreply.github.com>
2025-01-17 21:26:36 +0700

    Create setupwebphp.sh

---
+++ setupwebphp.sh
@@ -0,0 +1,9 @@
+clear
+echo "------------------------"
+echo "SNIPA VN"
+echo "YouTube : https://youtube.com/@snipavn205"
+echo "[ Code By SNIPA VN ]"
+echo "------------------------"
+
+
+https://github.com/Snipavn/Webhost-PHP/raw/refs/heads/main/WebsitePHPBYSnipavn.zip
