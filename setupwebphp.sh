3acfea221b9a18299a11ae1803fb58a12318ce00
Snipavn <108266321+Snipavn@users.noreply.github.com>
2025-01-17 21:36:19 +0700

    Update setupwebphp.sh

--- setupwebphp.sh
+++ setupwebphp.sh
@@ -1,9 +1,17 @@
 clear
+YELLOW='\033[1;33m'
 echo "------------------------"
 echo "SNIPA VN"
 echo "YouTube : https://youtube.com/@snipavn205"
+echo "Discord : https://dsc.gg/servertipacvn"
 echo "[ Code By SNIPA VN ]"
 echo "------------------------"
 
-
-https://github.com/Snipavn/Webhost-PHP/raw/refs/heads/main/WebsitePHPBYSnipavn.zip
+clear
+mkdir PhpWebhost
+echo -e " ${YELLOW} Thư mục mới tạo tên là PhpWebhost "
+echo " Bắt đầu tải xuống.... "
+cd PhpWebhost && curl -O WebsitePHPBYSnipavn.zip https://github.com/Snipavn/Webhost-PHP/raw/refs/heads/main/WebsitePHPBYSnipavn.zip
+echo " Tải thành công "
+echo " Lưu Ý phải cài ngrok để vào được trang web code do chủ kênh để nha"
+ls
