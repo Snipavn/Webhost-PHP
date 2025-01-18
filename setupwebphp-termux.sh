ce41a9b26f3dc04388dd08268bd009a49fd14d3f
Snipavn <108266321+Snipavn@users.noreply.github.com>
2025-01-18 09:57:08 +0700

    Update setupwebphp-termux.sh

--- setupwebphp-termux.sh
+++ setupwebphp-termux.sh
@@ -9,6 +9,7 @@ echo "------------------------"
 echo "------------------------"
 echo "Lưu ý code chỉ dành cho termux"
 echo "Nếu bạn dùng lệnh này lại thì nó sẽ xoá thư mục PhpWebhost"
+echo "Nếu banh dùng lệnh này thì bạn chưa tải package và ngrok"
 echo "Chờ 5s để vào cài đặt"
 sleep 5
 clear
@@ -28,16 +29,6 @@ pkg install unzip
 apt install php
 apt install nginx
 echo " Đã Tải xong Package"
-cd
-rm -rf PhpWebhost
-mkdir PhpWebhost
-echo -e " ${YELLOW} Thư mục mới tạo tên là PhpWebhost "
-echo " Bắt đầu tải xuống.... "
-cd PhpWebhost && curl -o a.zip https://download1591.mediafire.com/9e1oz3rqadxgwuTweE6tPkwx1m6JDCuRl4UVi8FdoYOGEPXBVBw0khe9JZSy_o7rKpG2DbhVogU2j15R2m7Ql1BifF0jrbWCx895w3N60ukXK9oypiCieooBqbvtXea1p6K2sOJq2zwrI3j9lFO3XVeNqDyuLksEeF2Jgr2TaKQ/jl45ss6u692safe/WebhostPHPBySnipavn.zip
-unzip a.zip && rm a.zip && rm start.sh && curl -o start.sh https://raw.githubusercontent.com/Snipavn/Webhost-PHP/refs/heads/main/start.sh
-cd
-echo " Tải thành công "
-ls
 clear
 echo "------------------------"
 echo "SNIPA VN"
@@ -47,5 +38,5 @@ echo "[ Code By SNIPA VN ]"
 echo "------------------------"
 echo "------------------------"
 echo "Done ✔️"
-echo "Hãy mở 1 session mới termux rồi nhập lệnh 'cd termux-ngrok && ./ngrok http 8080'"
-echo "Sau khi mở ngrok bên session mới rồi thì hãy quay lại session cũ , muốn khới động website nhập lệnh 'cd /PhpWebhost/ && sh start.sh' . Làm sao để tắt Website , Ấn Ctrl + C để tắt"
+echo "Hãy mở 1 session mới termux rồi nhập lệnh 'cd termux-ngrok && ./ngrok http 8080'.Nhớ treo không được tắt hoặc có thể bạn muốn tắt"
+echo "Sau khi mở ngrok bên session mới rồi thì hãy quay lại session cũ ,Nhập lệnh thứ 2 để tải Webhost-PHP ở https://github.com/Snipavn/Webhost-PHP/. Vào Phần Readme để chép lệnh thứ 2"
