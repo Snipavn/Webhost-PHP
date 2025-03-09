b807d1d47f0917b769aba639d5cfd139363dd06f
Snipavn <108266321+Snipavn@users.noreply.github.com>
2025-03-09 14:32:18 +0700

    Update setupwebphp-termux-package.sh

--- setupwebphp-termux-package.sh
+++ setupwebphp-termux-package.sh
@@ -16,6 +16,7 @@ clear
 echo " Đang tải ngrok và các package khác...."
 apt update -y && apt upgrade
 pkg update -y
+pkg install wget
 pkg install git
 git clone https://github.com/Yisus7u7/termux-ngrok
 cd termux-ngrok
